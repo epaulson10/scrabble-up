ce019f0d3820d64e9b0d70a937f9104f8e46cf50
smbeyer88 <smbeyer88@localhost>
2011-11-09 09:56:39 -0800

    Reformatted comments.

--- Dictionary.java
+++ Dictionary.java
@@ -3,22 +3,29 @@ package scrabble;
 import java.util.*;
 
 /** Dictionary
-
-Contains  a list of words that are considered legal. Contains the
-words dictated by TWL.*/
+ * 
+ * Contains  a list of words that are considered legal. Contains the
+ * words dictated by TWL.
+ */
 public class Dictionary {
-    public static HashSet<String> wordList;
 
-    public Dictionary() {
-        
-    }
+	public static HashSet<String> wordList;
 
-    /** checks to see if the word submitted is inside of the wordList.
-@param word - A String that is equal to the word to be checked.
-@return true of word is contained in the wordList. False otherwise.
-public static boolean isValidWord(String word) */
-    public static boolean isValidWord (String word) {
-        return false;
-    }
+	/**
+	 * Constructor
+	 */
+	public Dictionary() {
+
+	}
+
+	/** 
+	 * checks to see if the word submitted is inside of the wordList.
+	 * @param word - A String that is equal to the word to be checked.
+	 * @return true of word is contained in the wordList. False otherwise.
+	 * public static boolean isValidWord(String word) 
+	 */
+	public static boolean isValidWord (String word) {
+		return false;
+	}
 
 }
