3d4f31dc14954f3ed66d99a542e30d1fe568ed12
smbeyer88 <smbeyer88@localhost>
2011-11-09 12:03:58 -0800

    Implemented the dictionary class: the constructor initiates the hashSet from the TWL using the "word_list.txt" file. Implemented the isValidWord() method which uses the contains() method from hashSet to see if a given word is in the hashSet wordList.

--- Dictionary.java
+++ Dictionary.java
@@ -1,21 +1,33 @@
 package scrabble;
 
 import java.util.*;
+import java.io.*;
 
 /** Dictionary
  * 
  * Contains  a list of words that are considered legal. Contains the
- * words dictated by TWL.
+ * words dictated by Tournament Word List (TWL).
  */
 public class Dictionary {
 
 	public static HashSet<String> wordList;
 
-	/**
-	 * Constructor
+	/** 
+	 * Initializes the hashSet wordList to include all of the words
+	 * within the TWL. Takes approximately 326 milliseconds.
+	 * @throws IOException 
 	 */
-	public Dictionary() {
-
+	public Dictionary() throws IOException 
+	{
+		// bufferReader for the file containing the TWL reading one word per
+		// line and storing it into the hashSet
+		BufferedReader in = new BufferedReader(new FileReader("word_list.txt"));
+		String word = in.readLine();
+		while(word != null)
+		{
+			wordList.add(word);
+			word = in.readLine();
+		}
 	}
 
 	/** 
@@ -24,8 +36,10 @@ public class Dictionary {
 	 * @return true of word is contained in the wordList. False otherwise.
 	 * public static boolean isValidWord(String word) 
 	 */
-	public static boolean isValidWord (String word) {
-		return false;
+	public static boolean isValidWord (String word) 
+	{
+		if(wordList.contains(word)) return true;
+		else return false;
 	}
 
 }
