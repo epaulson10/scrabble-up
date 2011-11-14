dba46d36d809ba0cd727c7ce4851564a531b2741
Erik Paulson <epaulson10@gmail.com>
2011-11-14 11:01:56 -0800

    Added a dummy ScrabbleProxyPlayer class to get rid of the compiler error. Apparently this class didn't auto generate.

---
+++ ScrabbleProxyPlayer.java
@@ -0,0 +1,31 @@
+package scrabble;
+
+import game.GameAction;
+import game.GameState;
+import game.ProxyPlayer;
+
+public class ScrabbleProxyPlayer extends ProxyPlayer
+{
+
+    @Override
+    protected String encodeState(GameState gs)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected GameAction decodeAction(String s)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected int getAdmPortNum()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
