0adc5e0e983e191682348292139bcb7251843ca2
Aaron <Aaron@localhost>
2011-11-14 12:31:34 -0800

    Added ScrabbleProxyPlayer comments/method stubs/etc from auto-generated code. It did get autogenerated, must have been dropped somewhere along the line.

--- ScrabbleProxyPlayer.java
+++ ScrabbleProxyPlayer.java
@@ -1,31 +1,58 @@
 package scrabble;
 
-import game.GameAction;
-import game.GameState;
-import game.ProxyPlayer;
+import game.*;
+import java.util.*;
 
-public class ScrabbleProxyPlayer extends ProxyPlayer
-{
+/**Class representing a player playing over the network.*/
+class ScrabbleProxyPlayer extends ProxyPlayer implements ScrabblePlayer {
 
-    @Override
-    protected String encodeState(GameState gs)
-    {
-        // TODO Auto-generated method stub
+    /** Constructor - Calls super */
+    public ScrabbleProxyPlayer () {
+        super();
+    }
+
+    /**
+     * Decodes information about game actions that was encoded to be sent over 
+     * the network and returns a game action.
+     * @param s The string containing the encoded gameAction
+     * @return the decoded gameAction
+     */
+    protected GameAction decodeAction (String s) {
         return null;
     }
 
-    @Override
-    protected GameAction decodeAction(String s)
-    {
-        // TODO Auto-generated method stub
+    /** Encodes the updated game state after a network player makes a move
+to send the information to the next player
+
+@param gs the GameState being encoded
+@return A string containing the encoded game state */
+    protected String encodeState (GameState gs) {
         return null;
     }
 
-    @Override
-    protected int getAdmPortNum()
+    /**
+     * Returns the port number used for the network game mode
+     * @return the port number being used
+     */
+    protected int getAdmPortNum () {
+        return -1;
+    }
+
+    /** Adds tiles to the player's hand.
+
+    @param tilesToAdd  the tiles to add, contained in an array */
+    public void updateHand (Vector<ScrabbleTile> tilesToAdd) {
+        
+    }
+    
+    /**
+     *  Retrieves the player's hand.
+     *  
+     *  @return a copy of player's hand
+     */
+    public Vector<ScrabbleTile> getHand()
     {
-        // TODO Auto-generated method stub
-        return 0;
+        return null;
     }
 
 }
