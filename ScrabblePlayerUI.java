863239e83c52d2595d7c267a824ccd2c7ed52203
Erik Paulson <epaulson10@gmail.com>
2011-11-17 19:25:44 -0800

    Made various changes to ScrabblePlayerUI and ScrabbleTile to be able to draw tiles.

--- ScrabblePlayerUI.java
+++ ScrabblePlayerUI.java
@@ -6,15 +6,20 @@ import java.awt.*;
 /** A canvas that draws a Scrabble Game State
  * @author Aaron Dobbe, Steven Beyer, Erik Paulson, and Andrew Meyer
  * @version Yet to be implemented
+ * 
+ * NOTE: Find x,y values of where you clicked and divide by 15 = position.
  */
 public class ScrabblePlayerUI extends JPanel 
 {
-    private final static int BOARD_SIZE = 15;
-    private final static int TILE_SIZE = 35;
-    private final static int UI_SIZE = BOARD_SIZE*TILE_SIZE+1;
+    public final static int BOARD_SIZE = 15;
+    public final static int TILE_SIZE = 35;
+    public final static int UI_SIZE = BOARD_SIZE*TILE_SIZE+1;
+    //The amount of space between the board and the hand.
+    private final static int SPACE = 20;
     private ScrabbleGame model;
     private ScrabbleHumanPlayer player;
     private ScrabbleGameState state;
+    private ScrabbleTile test;
 
     /** 
      * Paints the game state
@@ -32,6 +37,16 @@ public class ScrabblePlayerUI extends JPanel
         {
             g.drawLine(0, TILE_SIZE*i, UI_SIZE, TILE_SIZE*i);
         }
+        
+        g.drawLine(4*TILE_SIZE, TILE_SIZE*15+SPACE, TILE_SIZE*11, TILE_SIZE*15+SPACE);
+        //Draw Bottom line
+        g.drawLine(4*TILE_SIZE, TILE_SIZE*16+SPACE, TILE_SIZE*11, TILE_SIZE*16+SPACE);
+        for (int i = 4; i <= 11; i++)
+        {
+            g.drawLine(TILE_SIZE*i, TILE_SIZE*15+SPACE, TILE_SIZE*i, TILE_SIZE*16+SPACE);
+        }
+        
+        g.drawImage(test.getPicture(), 0,0,TILE_SIZE, TILE_SIZE,null,null);
     }
 
     /** constructor
@@ -41,10 +56,11 @@ public class ScrabblePlayerUI extends JPanel
     public ScrabblePlayerUI (ScrabbleHumanPlayer initPlayer) 
     {
         player = initPlayer;
-        this.setSize(UI_SIZE, UI_SIZE);
+        this.setSize(UI_SIZE, UI_SIZE+TILE_SIZE+SPACE+1);
         this.setPreferredSize(this.getSize());
         this.setMinimumSize(this.getSize());
         this.setBackground(Color.lightGray);
+        test = new ScrabbleTile('A',1,false);
     }
 
     /**  * Updates the stored game state. */
